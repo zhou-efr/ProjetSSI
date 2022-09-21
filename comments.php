626f93bd3dcddc3ab1487053c45b25641a610f1f
zhou-efr <killian.zhou@efrei.net>
2022-09-21 22:04:56 +0200

    Comment page and index update

---
+++ comments.php
@@ -0,0 +1,112 @@
+<header>
+    <title>Super safe website</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</header>
+<body class="w-screen h-screen flex flex-col justify-center items-center">
+<div class="min-h-screen w-2/3 m-5 pb-14 flex flex-col items-center">
+    <?php
+    session_start();
+    include "./user/conn.php";
+
+    $nothing = '';
+
+    if (isset($_POST['submit'])){
+        $username = $conn->real_escape_string($_POST['username']);
+        $comment = $conn->real_escape_string($_POST['comment']);
+
+        $sql = "select * from utilisateur where nomdutilisateur='$username'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $id = $result->fetch_assoc()['id'];
+            $sql = "insert into commentaires (nomdutilisateur, commentaire) values ('$id', '$comment')";
+            if ($conn->query($sql) === TRUE) {
+                $nothing = $nothing . "Comment successfully posted <br/>";
+            } else {
+                # TODO: delete dev errors
+                $nothing = $nothing . "Error: " . $sql . "<br>" . $conn->error;
+            }
+        } else {
+            $nothing = $nothing . "User not found <br/>";
+        }
+    }
+
+    function get_username($connection, $user_id){
+        $sql = "select * from utilisateur where id='$user_id'";
+        $result = $connection->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return $row['nomdutilisateur'];
+        } else {
+            return "Unknown";
+        }
+    }
+
+    ?>
+    <div class="mb-6">
+        <h1 class="text-5xl text-center">Super safe website</h1>
+        <p class="text-center">This website is super safe, you can trust us.</p>
+    </div>
+    <div class="<?php if (!$nothing) {echo 'hidden';} ?> w-1/2 rounded-lg bg-yellow-100 flex flex-col justify-center text-center p-5 relative m-6">
+        <p class="absolute top-5 left-5 text-xl">⚠️</p>
+        <?php
+        echo $nothing;
+        ?>
+    </div>
+    <div class="w-full flex justify-center items-center">
+        <form
+            action="comments.php"
+            method="post"
+            class="w-1/3 flex flex-col justify-center items-center"
+        >
+            <label for="username" class="font-bold tracking-wider">Username</label>
+            <input
+                type="text"
+                name="username"
+                required
+                class="w-2/3 border border-gray-400 p-2 pr-0 m-2 rounded"
+            />
+            <label for="comment" class="font-bold tracking-wider">Comment</label>
+            <textarea
+                name="comment"
+                required
+                class="w-2/3 border border-gray-400 p-2 pr-0 m-2 rounded"
+            ></textarea>
+            <button
+                type="submit"
+                name="submit"
+                value="submit"
+                class="w-1/3 bg-blue-500 hover:bg-blue-700 text-white p-2 m-2 rounded"
+            >
+                Comment
+            </button>
+        </form>
+        <div class="w-2/3 h-2/3 border border-gray-400 p-2 pr-0 m-2 rounded overflow-y-scroll overflow-x-hidden break-words">
+            <?php
+            $sql = "SELECT * FROM commentaires";
+            $result = $conn->query($sql);
+            while ($row = $result->fetch_assoc()) {
+                ?>
+                <div class="w-full flex items-start m-1 pr-6">
+                    <p class="font-bold tracking-wider w-1/5"><?php echo get_username($conn, $row['nomdutilisateur']); ?></p>
+                    <p class="ml-2 text-justify"><?php echo $row['commentaire']; ?></p>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+</div>
+<div class="h-14 fixed left-0 bottom-0 w-screen border-t border-gray-400 flex bg-white">
+    <div class="w-1/2 hover:underline flex flex-col justify-center items-center">
+        <a href="index.php">Home</a>
+    </div>
+    <div class="w-1/2 hover:underline flex flex-col justify-center items-center">
+        <a href="comments.php">Comments</a>
+    </div>
+</div>
+<?php
+$conn->close();
+?>
+</body>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </header>
 <body class="w-screen h-screen flex flex-col justify-center items-center">
-    <div class="h-screen w-2/3 m-5 flex flex-col items-center">
+    <div class="h-screen w-2/3 m-5 mb-14 flex flex-col items-center">
         <div class="mb-14">
             <h1 class="text-5xl text-center">Super safe website</h1>
             <p class="text-center">This website is super safe, you can trust us.</p>
@@ -20,7 +20,6 @@
         if (isset($_POST['register'])) {
             /** @noinspection PhpUndefinedVariableInspection */
             $username = $conn->real_escape_string($_POST['nomdutilisateur']);
-            $password = $conn->real_escape_string($_POST['motdepasse']);
             $donneespersonnelles = $conn->real_escape_string("Ullamco short loin leberkas voluptate dolore short ribs officia pig fugiat capicola enim ground round hamburger tail tempor.");
 
             $sql = "select * from utilisateur where nomdutilisateur='$username'";
@@ -30,7 +29,7 @@
             if ($result->num_rows > 0) {
                 $nothing = $nothing . "User already registered <br/>";
             } else {
-                $sql = "insert into utilisateur (nomdutilisateur, motdepasse, donneespersonnelles) values ('$username', '".md5($password)."', '$donneespersonnelles')";
+                $sql = "insert into utilisateur (nomdutilisateur, donneespersonnelles) values ('$username', '$donneespersonnelles')";
                 if ($conn->query($sql) === TRUE) {
                     $nothing = $nothing . "User successfully registered <br/>";
                     $logging = true;
@@ -43,9 +42,8 @@
 
         if ($logging) {
             $username = $conn->real_escape_string($_POST['nomdutilisateur']);
-            $password = $conn->real_escape_string($_POST['motdepasse']);
 
-            $sql = "SELECT * FROM utilisateur WHERE nomdutilisateur = '$username' AND motdepasse = '".md5($password)."'";
+            $sql = "SELECT * FROM utilisateur WHERE nomdutilisateur = '$username'";
             $result = $conn->query($sql);
 
             if($result->num_rows > 0) {
@@ -58,17 +56,81 @@
             } else {
                 $nothing = $nothing . "Failed to login <br/>";
             }
-        }elseif (isset($_SESSION['id'])) {
-            $nothing . "Logged as " . $_SESSION['nomdutilisateur'] . " <br/>";
+        }
+
+        if (isset($_POST['delete'])) {
+            $username = $conn->real_escape_string($_POST['nomdutilisateur']);
+
+            $sql = "select * from utilisateur where nomdutilisateur='$username'";
+
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $id = $row['id'];
+
+                $sql = "delete from utilisateur where id='$id' and nomdutilisateur='$username'";
+                if ($conn->query($sql) === TRUE) {
+                    $nothing = $nothing . "Successfully delete " . $username . " <br/>";
+                } else {
+                    # TODO: delete dev errors
+                    $nothing = $nothing . "Error: " . $sql . "<br>" . $conn->error;
+                }
+            } else {
+                $nothing = $nothing . "User " . $username . " doesn\'t exist <br/>";
+            }
         }
         ?>
+
+        <form
+                action="./index.php"
+                method="post"
+                class="w-full flex flex-col justify-center items-center"
+        >
+            <label for="username" class="font-bold tracking-wider">Username</label>
+            <input
+                    type="text"
+                    name="nomdutilisateur"
+                    id="username"
+                    required
+                    class="w-2/3 border border-gray-400 p-2 pr-0 m-2 rounded"
+            >
+
+            <div class="flex justify-center w-full">
+                <button
+                        type="submit"
+                        name="login"
+                        value="login"
+                        class="w-1/3 bg-blue-500 hover:bg-blue-700 text-white p-2 m-2 rounded"
+                >
+                    Show data
+                </button>
+
+                <button
+                        type="submit"
+                        name="register"
+                        value="register"
+                        class="w-1/3 bg-blue-500 hover:bg-blue-700 text-white p-2 m-2 rounded"
+                >
+                    Sign up
+                </button>
+
+                <button
+                        type="submit"
+                        name="delete"
+                        value="delete"
+                        class="w-1/3 bg-red-500 hover:bg-red-700 text-white p-2 m-2 rounded"
+                >
+                    Delete
+                </button>
+            </div>
+        </form>
         <div class="<?php if (!$nothing) {echo 'hidden';} ?> w-1/2 rounded-lg bg-yellow-100 flex flex-col justify-center text-center p-5 relative m-10">
             <p class="absolute top-5 left-5 text-xl">⚠️</p>
             <?php
             echo $nothing;
             ?>
         </div>
-
         <?php
             if(isset($_SESSION['id'])){
                 ?>
@@ -76,6 +138,7 @@
                     <p class="absolute top-5 left-5 text-xl">🔓️</p>
                     <p class="mx-14">
                         <?php
+                        echo "Logged as " . $_SESSION['nomdutilisateur'] . " <br/>";
                         echo $_SESSION['donneespersonnelles'];
                         ?>
                     </p>
@@ -85,54 +148,16 @@
                     <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded m-5" type="submit">Logout</button>
                 </form>
                 <?php
-            }else{
-                ?>
-                <form
-                        action="./index.php"
-                        method="post"
-                        class="w-full flex flex-col justify-center items-center"
-                >
-                    <label for="username" class="font-bold tracking-wider">Username</label>
-                    <input
-                            type="text"
-                            name="nomdutilisateur"
-                            id="username"
-                            required
-                            class="w-2/3 border border-gray-400 p-2 pr-0 m-2 rounded"
-                    >
-
-                    <label for="password" class="font-bold tracking-wider">Password</label>
-                    <input
-                            type="password"
-                            name="motdepasse"
-                            id="password"
-                            required
-                            class="w-2/3 border border-gray-400 p-2 pr-0 m-2 rounded"
-                    >
-
-                    <div class="flex justify-center w-full">
-                        <button
-                                type="submit"
-                                name="login"
-                                value="login"
-                                class="w-1/2 bg-blue-500 text-white p-2 m-2 rounded"
-                        >
-                            Login
-                        </button>
-
-                        <button
-                                type="submit"
-                                name="register"
-                                value="register"
-                                class="w-1/2 bg-blue-500 text-white p-2 m-2 rounded"
-                        >
-                            Sign up
-                        </button>
-                    </div>
-                </form>
-                <?php
             }
         ?>
+    </div>
+    <div class="h-14 fixed left-0 bottom-0 w-screen border-t border-gray-400 flex bg-white">
+        <div class="w-1/2 hover:underline flex flex-col justify-center items-center">
+            <a href="index.php">Home</a>
+        </div>
+        <div class="w-1/2 hover:underline flex flex-col justify-center items-center">
+            <a href="comments.php">Comments</a>
+        </div>
     </div>
     <?php
         $conn->close();
