9732d676e7b19358c027cea1f5c8d75fb46a513f
zhou-efr <kilian.zhou@efrei.net>
2022-09-24 21:00:16 +0000

    prod init message

--- AsJoe/user/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "Joe", "tZM6OyPrS.QPVk(U", "ProjetSSI");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-if (!$conn) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$conn->set_charset("utf8");
