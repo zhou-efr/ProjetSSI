a839fa268581429d22e22e331e3c9dd88b75189f
zhou-efr <killian.zhou@efrei.net>
2022-09-25 14:03:03 +0000

    error_logging

--- user/conn.php
+++ user/conn.php
@@ -1,9 +1,9 @@
 <?php
 $conn = new mysqli("10.0.1.6", "SGBD", "g489Z15n!mM34JKISZZL", "ProjetSSI");
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    error_log("Connection failed: " . $conn->connect_error);
 }
 if (!$conn) {
-    die("Connection failed: " . $conn->connect_error);
+    error_log("Connection failed: " . $conn->connect_error);
 }
 $conn->set_charset("utf8");
